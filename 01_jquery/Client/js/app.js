482734669ed428a2b3ffd7ac807553e15ea5f390
johnkao3 <johnkao3@gmail.com>
2018-07-31 15:33:25 +0800

    07/27

---
+++ 01_jquery/Client/js/app.js
@@ -0,0 +1,30 @@
+function changeGreen() {
+  $("#header").attr('class', 'text-green');
+  index = 0;
+}
+
+function changeBlue() {
+  $("#header").attr('class', 'text-blue');
+  index = 1;
+}
+
+// If index = 0 is blue
+// Else is Green
+function changeColor() {
+  if( index == 0) {
+    changeBlue()
+  }
+  else {
+    changeGreen()
+  }
+}
+
+function main() {
+  $("#header").on("click", changeColor);
+  $("#btnBlue").on("click", changeBlue);
+  $("#btnGreen").on("click", changeGreen);
+  changeGreen();
+}
+
+var index = 0;
+$(document).ready( main );
