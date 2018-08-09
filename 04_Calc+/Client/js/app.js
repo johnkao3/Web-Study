8709afef9084a24605af340289737bfca65663ab
johnkao3 <johnkao3@hotmail.com>
2018-08-09 16:48:19 +0800

    Add setInput Funcrion Pre

--- 04_Calc+/Client/js/app.js
+++ 04_Calc+/Client/js/app.js
@@ -1,5 +1,27 @@
+function setInput(e) {
+  var vID = this.id.substring(3);
+  var vExpr = $("#expr").val();
+  var vExprLen = vExpr.length;
+  var vOp = {
+    "Add": "+",
+    "Sub": "-",
+    "Mlt": "*",
+    "Dib": "/",
+    "Dot": ".",
+    "L"  : "(",
+    "R"  : ")"
+  };
+  switch ( vID ) {
+    case "Cls" : 
+      $("#expr").val("");
+      $("#ans").val("");
+      break;
+     
+  }
+}
 function main() {
   $('.calcBtn').button();
+  $('.calcBtn').on('click', setInput);
 }
 
 $(document).ready( main );
